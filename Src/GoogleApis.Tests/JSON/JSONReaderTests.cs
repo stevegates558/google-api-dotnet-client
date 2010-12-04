75cba316f86f226fe1c6ce8ab626407bf8a8a092
David Waters <davidwaters@google.com>
2010-12-04 15:53:54 +0000

    JSON => Json as per MS & google best practices. Fix a couple of compile warnings

--- Src/GoogleApis.Tests/JSON/JSONReaderTests.cs
+++ Src/GoogleApis.Tests/JSON/JSONReaderTests.cs
@@ -1,4 +1,19 @@
-using Google.Apis.JSON;
+/*
+Copyright 2010 Google Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+using Google.Apis.Json;
 
 using NUnit.Framework;
 using System.Collections.Generic;
@@ -13,7 +28,7 @@ namespace Google.Apis.Tests
     ///to contain all TokenStream_JSONReaderTest Unit Tests
     ///</summary>
   [TestFixture()]
-  public class JSONReaderTest {
+  public class JsonReaderTest {
 
 
 
@@ -24,20 +39,20 @@ namespace Google.Apis.Tests
     [Test()]
     public void ParseTest() {
       string jsonAsText = "[ true, \"Value2\", false, null ]";
-      object o = JSONReader.Parse(jsonAsText);
+      object o = JsonReader.Parse(jsonAsText);
       Assert.IsTrue(o is ArrayList);
       ArrayList a = o as ArrayList;
       Assert.AreEqual(4, a.Count);
       Assert.AreEqual(false, a[2]);
 
       jsonAsText = "{ \"Name\" : true }";
-      o = JSONReader.Parse(jsonAsText);
-      Assert.IsTrue(o is JSONDictionary);
+      o = JsonReader.Parse(jsonAsText);
+      Assert.IsTrue(o is JsonDictionary);
 
       jsonAsText = "{ \"name\" : [ true, \"Value2\", false ], \"sub\" : { \"subname\" : 1234 } }";
-      o = JSONReader.Parse(jsonAsText);
-      Assert.IsTrue(o is JSONDictionary);
-      JSONDictionary j = o as JSONDictionary;
+      o = JsonReader.Parse(jsonAsText);
+      Assert.IsTrue(o is JsonDictionary);
+      JsonDictionary j = o as JsonDictionary;
       a = j["name"] as ArrayList;
       Assert.IsNotNull(a);
   
